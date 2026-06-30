6656abee2bbf280f79c7ca51725c14eb05724ba1
Oleh <olesho@gmail.com>
2026-06-30 21:07:43 +0200

    META-HARNESS-2: add farewell() helper and test

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,6 +1,10 @@
 import { expect, test } from "bun:test"
-import { greet } from "../src/index.ts"
+import { farewell, greet } from "../src/index.ts"
 
 test("greet builds a greeting", () => {
   expect(greet("world")).toBe("Hello, world!")
+})
+
+test("farewell builds a farewell", () => {
+  expect(farewell("world")).toBe("Goodbye, world!")
 })
